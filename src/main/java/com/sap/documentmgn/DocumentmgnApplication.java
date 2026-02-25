a5379ec7f2655eadead11dd20cf7a09d383b9592
Georgi Kolev <georgi.kolev01@sap.com>
2026-02-25 18:24:50 +0200

    init

---
+++ src/main/java/com/sap/documentmgn/DocumentmgnApplication.java
@@ -0,0 +1,13 @@
+package com.sap.documentmgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentmgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentmgnApplication.class, args);
+	}
+
+}

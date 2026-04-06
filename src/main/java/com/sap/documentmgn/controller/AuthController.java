6f7a6baafb74b129422d53eddd867a485c2d9a38
tanyapetkova19 <tanyapetkova192@gmail.com>
2026-04-06 22:57:30 +0300

    Added user registration, login logic, and security configurations

---
+++ src/main/java/com/sap/documentmgn/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.sap.documentmgn.controller;
+
+import com.sap.documentmgn.dto.LoginRequestDTO;
+import com.sap.documentmgn.dto.UserDTO;
+import com.sap.documentmgn.dto.UserRegistrationDTO;
+import com.sap.documentmgn.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserRegistrationDTO registrationDTO){
+        UserDTO response = userService.registerUser(registrationDTO);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDTO.getUsername(), loginRequestDTO.getPassword())
+        );
+        return ResponseEntity.ok("Login successful");
+    }
+
+}

e999112a1adc44d5777ed99937e3e24006861bbf
tanyapetkova19 <tanyapetkova192@gmail.com>
2026-04-14 19:18:57 +0300

    configure test environment and disable security for CI tests

--- src/test/java/com/sap/documentmgn/DocumentmgnApplicationTests.java
+++ src/test/java/com/sap/documentmgn/DocumentmgnApplicationTests.java
@@ -1,9 +1,12 @@
 package com.sap.documentmgn;
 
+import com.sap.documentmgn.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestSecurityConfig.class)
 class DocumentmgnApplicationTests {
 
 	@Test

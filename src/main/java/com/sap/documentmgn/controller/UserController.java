115f7f0f4262b3e456bc8655b1812de9e069cf8e
Yoanna <yoanna.yordanova2402@gmail.com>
2026-04-20 19:32:09 +0300

    secure and validate UserController, DocumentHistoryController

--- src/main/java/com/sap/documentmgn/controller/UserController.java
+++ src/main/java/com/sap/documentmgn/controller/UserController.java
@@ -6,6 +6,7 @@ import com.sap.documentmgn.entity.ROLES;
 import com.sap.documentmgn.service.DocumentService;
 import com.sap.documentmgn.service.UserService;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,29 +27,33 @@ public class UserController {
         this.documentService = documentService;
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/api/v1/users")
     public List<UserDTO> getUsers(){
         return userService.getUsers();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/api/v1/users/{page}")
-    public List<UserDTO> getUsersByTen(@PathVariable int page){
+    public List<UserDTO> getUsersByTen(@PathVariable @Min(0) int page){
         return userService.getUsersByTen(page);
     }
 
-
+    @PreAuthorize("hasAnyRole('ADMIN', 'AUTHOR', 'REVIEWER')")
     @GetMapping("/api/v1/users/versions/{page}")
-    public List<DocumentVersionDTO> getAllTeamVersionsPage(@PathVariable int page) {
+    public List<DocumentVersionDTO> getAllTeamVersionsPage(@PathVariable @Min(0) int page) {
         return userService.getAllTeamVersionsPage(page);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'AUTHOR', 'REVIEWER')")
     @GetMapping("/api/v1/users/{userId}/versions")
-    public List<DocumentVersionDTO> getAllUserVersions(@PathVariable Long userId) {
+    public List<DocumentVersionDTO> getAllUserVersions(@PathVariable @Min(1) Long userId) {
         return userService.getAllUserVersions(userId);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'AUTHOR', 'REVIEWER')")
     @GetMapping("/api/v1/users/{userId}/versions/{page}")
-    public List<DocumentVersionDTO> getAllUserVersionsPage(@PathVariable Long userId, @PathVariable int page) {
+    public List<DocumentVersionDTO> getAllUserVersionsPage(@PathVariable @Min(1) Long userId, @PathVariable @Min(0) int page) {
         return userService.getAllUserVersionsPage(userId, page);
     }
 
@@ -62,7 +67,7 @@ public class UserController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/api/v1/admin/deleteUser/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable @Min(1) Long userId, @NonNull Principal principal) {
+    public ResponseEntity<?> deleteUser(@PathVariable @Min(1) Long userId, @NotNull Principal principal) {
         String initUsername = principal.getName();
         userService.deleteUser(userId, initUsername);
         return ResponseEntity.ok().build();
